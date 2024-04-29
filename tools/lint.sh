445cff5f56c3486f45e725ed164dbc58d55285b1
Jonathan Dowland <jdowland@redhat.com>
2024-04-29 14:15:03 +0100

    fixup! WIP: Extend GHA with a YAML/JSON linter

--- tools/lint.sh
+++ tools/lint.sh
@@ -1,7 +1,8 @@
 ret=0
 
 # XXX this will only work for one commit PRs. Need to diff against the
-# merge base
+# merge base. Note also GHA checkout@v4 will only fetch the top-most
+# commit by default!
 git diff-tree --no-commit-id --name-only HEAD -r |\
 while read f; do
   case "${f##*.}" in
