9bfedfec9cfc81c2c359152c46a4b1134855a2fc
Jonathan Dowland <jdowland@redhat.com>
2025-08-27 15:57:53 +0100

    [OPENJDK-4024] adjust permissions on jboss-settings.xml
    Wildfly, who use our image sources, require group-write permission
    to this file.
    
    Signed-off-by: Jonathan Dowland <jdowland@redhat.com>

--- modules/maven/default/configure.sh
+++ modules/maven/default/configure.sh
@@ -4,8 +4,9 @@ set -euo pipefail
 SCRIPT_DIR=$(dirname $0)
 ARTIFACTS_DIR=${SCRIPT_DIR}/artifacts
 
-install -m 0644 -D -t /opt/jboss/container/maven/default \
-    ${ARTIFACTS_DIR}/opt/jboss/container/maven/default/*
+install -m 0644 -D {${ARTIFACTS_DIR},}/opt/jboss/container/maven/default/maven.sh
+# 0664: OPENJDK-4024
+install -m 0664 -D {${ARTIFACTS_DIR},}/opt/jboss/container/maven/default/jboss-settings.xml
 
 MAVEN_VERSION_SQUASHED=${MAVEN_VERSION/./}
 
