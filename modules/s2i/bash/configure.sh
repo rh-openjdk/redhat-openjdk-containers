21c64557fa52af71942f2a60be415f6ffb3f705d
Jonathan Dowland <jdowland@redhat.com>
2025-08-28 12:41:40 +0100

    [OPENJDK-4025] Revert "Merge pull request #559 from jmtd/ubi10-test-artifacts"
    This reverts commit f127de2fadc1fbd3af5bac5fc3e92a3f198f67a3, reversing
    changes made to df4546641c641242236229f06b3218aa145c7a76.

--- modules/s2i/bash/configure.sh
+++ modules/s2i/bash/configure.sh
@@ -7,12 +7,9 @@ ARTIFACTS_DIR=${SCRIPT_DIR}/artifacts
 
 chown -R $USER:root $SCRIPT_DIR
 chmod -R ug+rwX $SCRIPT_DIR
-chmod ug+x ${ARTIFACTS_DIR}/opt/jboss/container/s2i/core/*
+chmod ug+x ${ARTIFACTS_DIR}/opt/jboss/container/java/s2i/*
+chmod ug+x ${ARTIFACTS_DIR}/usr/libexec/s2i/*
 
 pushd ${ARTIFACTS_DIR}
 cp -pr * /
 popd
-
-mkdir -p /deployments \
- && chmod -R "ug+rwX" /deployments \
- && chown -R $USER:root /deployments
