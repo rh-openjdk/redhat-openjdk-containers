f19deead665d01123954b469153d7e58165f50a2
Jonathan Dowland <jdowland@redhat.com>
2025-09-03 12:01:06 +0100

    [OPENJDK-4030] remove JVM-specific diagnostics
    This mechanism allows for different JVM diagnostic flags, but we
    are using the same values for all supported JDK versions.
    
    Signed-off-by: Jonathan Dowland <jdowland@redhat.com>

--- modules/jdk/21/configure.sh
+++ modules/jdk/21/configure.sh
@@ -1,17 +1,5 @@
-#!/bin/sh
-# Configure module
-set -e
-
-SCRIPT_DIR=$(dirname $0)
-ARTIFACTS_DIR=${SCRIPT_DIR}/artifacts
-
-chown -R $USER:root $SCRIPT_DIR
-chmod -R ug+rwX $SCRIPT_DIR
-chmod ug+x ${ARTIFACTS_DIR}/opt/jboss/container/openjdk/jdk/*
-
-pushd ${ARTIFACTS_DIR}
-cp -pr * /
-popd
+#!/bin/bash
+set -euo pipefail
 
 # Update securerandom.source for quicker starts (must be done after removing jdk 8, or it will hit the wrong files)
 JAVA_SECURITY_FILE=/usr/lib/jvm/java/conf/security/java.security
