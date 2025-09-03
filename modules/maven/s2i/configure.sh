76fb69ea57d834da5294c7b52d1e5f02c1805a4c
Jonathan Dowland <jdowland@redhat.com>
2025-09-03 15:02:34 +0100

    Adjust install line to reflect only one file
    This line originates in ubi9 branch where there are two files in
    the source (save-artifacts and assemble). When there is only one
    file, as in the ubi10 branch, the install command copies it to the
    destination path instead of _underneath_ it.
    
    Signed-off-by: Jonathan Dowland <jdowland@redhat.com>

--- modules/maven/s2i/configure.sh
+++ modules/maven/s2i/configure.sh
@@ -4,6 +4,6 @@ set -euo pipefail
 SCRIPT_DIR=$(dirname $0)
 ARTIFACTS_DIR=${SCRIPT_DIR}/artifacts
 
-install -D ${ARTIFACTS_DIR}/usr/libexec/s2i/* /usr/libexec/s2i
+install -D {${ARTIFACTS_DIR},}/usr/libexec/s2i/save-artifacts
 install -m 0644 -D -t /opt/jboss/container/maven/s2i \
     ${ARTIFACTS_DIR}/opt/jboss/container/maven/s2i/*
