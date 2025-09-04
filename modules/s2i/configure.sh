a3dc587fbf231e93e9af9255aed05f28daa554a8
Jonathan Dowland <jdowland@redhat.com>
2025-09-04 09:21:48 +0100

    [OPENJDK-3666] merge jboss.container.java.s2i.{core,bash} modules
    Signed-off-by: Jonathan Dowland <jdowland@redhat.com>

--- modules/s2i/configure.sh
+++ modules/s2i/configure.sh
@@ -7,3 +7,9 @@ ARTIFACTS_DIR=${SCRIPT_DIR}/artifacts
 install -D ${ARTIFACTS_DIR}/usr/libexec/s2i/* /usr/libexec/s2i
 install -m 0644 -D -t /opt/jboss/container/java/s2i \
     ${ARTIFACTS_DIR}/opt/jboss/container/java/s2i/*
+
+install -m 0644 -D {${ARTIFACTS_DIR},}/opt/jboss/container/java/s2i/s2i-core
+
+mkdir -p /deployments \
+ && chmod -R "ug+rwX" /deployments \
+ && chown -R $USER:root /deployments
