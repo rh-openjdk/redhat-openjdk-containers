9cee4cf7e71d1a08d44cdf32291188cb4bca70ab
Jonathan Dowland <jdowland@redhat.com>
2025-12-11 10:46:38 +0000

    singleton-jdk: adjustments for EPEL
    Special-case handling of JAVA_VENDOR and JAVA_VERSION for the EPEL
    packages. This prevents singleton-jdk from removing all the java
    packages.
    
    Signed-off-by: Jonathan Dowland <jdowland@redhat.com>

--- modules/singleton-jdk/configure.sh
+++ modules/singleton-jdk/configure.sh
@@ -1,6 +1,6 @@
-#!/bin/sh
-set -u
-set -e
+#!/bin/bash
+set -ueo pipefail
+set -x
 
 if [ -z "$JAVA_VERSION" ]; then
   echo "JAVA_VERSION needs to be defined to use this module" >&2
@@ -11,6 +11,15 @@ if [ -z "$JAVA_VENDOR" ]; then
   exit 1
 fi
 
+# java-latest/EPEL does not follow the pattern expected
+if [ "$JAVA_VENDOR" = "epel" ]; then
+  JAVA_VERSION=latest
+  JAVA_VENDOR=openjdk
+fi
+
 # Clean up any java-* packages that have been installed that do not match
 # our stated JAVA_VERSION-JAVA_VENDOR (e.g.: 11-openjdk; 1.8.0-openj9)
 rpm -e --nodeps $(rpm -qa java-* | grep -v "^java-${JAVA_VERSION}-${JAVA_VENDOR}")
+
+# sanity check: there should still be a java installed
+command -v java
