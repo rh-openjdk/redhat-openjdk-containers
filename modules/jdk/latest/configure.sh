177d6ac45258c4e4deb05273eeef480db3d73265
Jonathan Dowland <jdowland@redhat.com>
2026-07-07 16:34:57 +0100

    [OPENJDK-4822] Remove EPEL, java-latest, redundant module spec in overrides
    Continuation of
    https://redhat.atlassian.net/browse/OPENJDK-4822
    
    Signed-off-by: Jonathan Dowland <jdowland@redhat.com>

--- modules/jdk/latest/configure.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-# Configure module
-set -e
-
-SCRIPT_DIR=$(dirname $0)
-ARTIFACTS_DIR=${SCRIPT_DIR}/artifacts
-
-install -m 0644 -D {${ARTIFACTS_DIR},}/opt/jboss/container/openjdk/jdk/jvm-options
--- modules/jre/latest/configure.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-set -euo pipefail
-
-SCRIPT_DIR=$(dirname $0)
-ARTIFACTS_DIR=${SCRIPT_DIR}/artifacts
-
-install -m 0644 -D {${ARTIFACTS_DIR},}/opt/jboss/container/openjdk/jre/jvm-options
